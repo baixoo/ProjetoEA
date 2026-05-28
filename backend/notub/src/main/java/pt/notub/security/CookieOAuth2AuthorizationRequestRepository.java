2299714e2d749950100124bd00e2377da04b9885
PaulAlv99 <2120722@student.uma.pt>
2026-05-28 20:29:14 +0100

    error with jackson..there's multiple jackson versions which is bad

--- backend/notub/src/main/java/pt/notub/security/CookieOAuth2AuthorizationRequestRepository.java
+++ backend/notub/src/main/java/pt/notub/security/CookieOAuth2AuthorizationRequestRepository.java
@@ -1,6 +1,6 @@
 package pt.notub.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
